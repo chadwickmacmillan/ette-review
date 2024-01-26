ad0bb0b62a2cbd70a57d2c0d397c666deb02b9bf
Chadwick Macmillan <chadwickmacmillan@gmail.com>
2024-01-25 23:57:33 -0500

    Updates

---
+++ src/editions/fall2023.js
@@ -0,0 +1,61 @@
+export const fall2023Writers = [
+  {
+    name: "Andrew Bertaina",
+    slug: "bertaina",
+    imageSrc: "whales",
+instagram: "",
+	  facebook: "",
+	  twitter: "",
+  },
+  {
+    name: "Patricia Q. Bidar",
+    slug: "bidar",
+    imageSrc: "whales",
+  },
+  {
+    name: "Avitus B. Carle",
+    slug: "carle",
+    imageSrc: "whales",
+  },
+  {
+    name: "Kathy Fish",
+    slug: "fish",
+    imageSrc: "whales",
+  },
+  {
+    name: "Eliot Li",
+    slug: "li",
+    imageSrc: "whales",
+  },
+  {
+    name: "Jennifer Martelli",
+    slug: "martelli",
+    imageSrc: "whales",
+  },
+  {
+    name: "Douglas A. Martin",
+    slug: "martin",
+    imageSrc: "whales",
+  },
+  {
+    name: "Cheryl Pappas",
+    slug: "pappas",
+    imageSrc: "whales",
+  },
+  {
+    name: "M.A. Scott",
+    slug: "scott",
+    imageSrc: "whales",
+  },
+  {
+    name: false,
+    slug: false,
+    imageSrc: "whales",
+  },
+  {
+    name: "pod-ette",
+    slug: "podette",
+    imageSrc: "whales",
+  },
+];
+
