ee0c8ad36829794b16779e96451d02f72b6b1f2e
Chadwick Macmillan <chadwickmacmillan@gmail.com>
2024-02-13 00:47:02 -0500

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  outDir: './public_html'
+  outDir: './public_html',
 	site: 'https://ettereview.com',
 });
