4957b7151370c9f50473560be9c5cf2bab7505b4
Chester Jankowski <chester.jankowski@gmail.com>
2024-09-26 13:33:45 -0400

    updates for fall2024

---
+++ src/editions/fall2024.js
@@ -0,0 +1,74 @@
+export const fall2024 = [
+	{
+		name: "Jonathan Fletcher",
+		slug: "fletcher",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/fletcher.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Kelle Groom",
+		slug: "groom",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/groom.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Bryana Joy",
+		slug: "joy",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/joy.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Veda Khan",
+		slug: "khan",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/khan.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Amy Marques",
+		slug: "marques",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/marques.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Kathleen McGookey",
+		slug: "mcgookey",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/mcgookey.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Susan Rich",
+		slug: "rich",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/rich.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Cathy Ulrich",
+		slug: "ulrich",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/ulrich.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: false,
+		slug: false,
+		imageSrc: import.meta.env.BASE_URL + "images/fall2024/leafpromo.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+];
