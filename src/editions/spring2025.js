5c6026e5f548a7e3df3efcc7dbfd2ef4ec1a6d2d
Chester Jankowski <chester.jankowski@gmail.com>
2025-05-01 18:23:34 -0400

    Spring 2025 review draft

---
+++ src/editions/spring2025.js
@@ -0,0 +1,56 @@
+export const spring2025 = [
+	{
+		name: "Jessica Barlevi",
+		slug: "barlevi",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/barlevi.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Mathieu Cailler",
+		slug: "cailler",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/cailler.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Amy Holman",
+		slug: "holman",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/holman.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Devan Murphy",
+		slug: "murphy",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/murphy.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "S. D. Pergola",
+		slug: "pergola",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/pergola.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "Nora Esme Wagner",
+		slug: "wagner",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/wagner.jpg",
+		instagram: "share",
+		twitter: "share",
+		facebook: "share"
+	},
+	{
+		name: "pod-ette",
+		slug: "podette",
+		link: "/podette",
+		imageSrc: import.meta.env.BASE_URL + "images/spring2025/podette.webp",
+	},
+];
