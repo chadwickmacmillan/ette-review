01b4d6bc40862fff5a6f5717de919dab96146d16
Chester Jankowski <chester.jankowski@gmail.com>
2025-09-25 19:41:39 -0400

    fall 2025 preview

---
+++ src/editions/fall2025.js
@@ -0,0 +1,38 @@
+export const fall2025 = [
+	{
+		name: "Laura Cronk",
+		slug: "cronk",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2025/cronk.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Jose Hernandez Diaz",
+		slug: "diaz",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2025/diaz.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Suzy Eynon",
+		slug: "eynon",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2025/eynon.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Rachel Kowalsky",
+		slug: "kowalsky",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2025/kowalsky.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Matt Leibel",
+		slug: "leibel",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2025/leibel.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Emma Raimondo",
+		slug: "raimondo",
+		imageSrc: import.meta.env.BASE_URL + "images/fall2025/raimondo.jpg",
+		bluesky: "share"
+	},
+];
