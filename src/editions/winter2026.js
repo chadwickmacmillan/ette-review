bec3c79dbecf375eebb278c23229db83639489c2
Chester Jankowski <chester.jankowski@gmail.com>
2026-01-29 14:45:02 -0500

    winter 2026 draft

---
+++ src/editions/winter2026.js
@@ -0,0 +1,38 @@
+export const winter2026 = [
+	{
+		name: "Lauren Camp",
+		slug: "camp",
+		imageSrc: import.meta.env.BASE_URL + "images/winter2026/camp.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Theodore Heil",
+		slug: "heil",
+		imageSrc: import.meta.env.BASE_URL + "images/winter2026/heil.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Marc Kaufman",
+		slug: "kaufman",
+		imageSrc: import.meta.env.BASE_URL + "images/winter2026/kaufman.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Dawn Macdonald",
+		slug: "macdonald",
+		imageSrc: import.meta.env.BASE_URL + "images/winter2026/macdonald.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Natalie Marino",
+		slug: "marino",
+		imageSrc: import.meta.env.BASE_URL + "images/winter2026/marino.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Adam McOmber",
+		slug: "mcomber",
+		imageSrc: import.meta.env.BASE_URL + "images/winter2026/mcomber.jpg",
+		bluesky: "share"
+	},
+];
