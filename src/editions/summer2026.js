90fb89d9c103e0ad90ca4b6a41b58ee9716e30a1
Chester Jankowski <chester.jankowski@proton.me>
2026-07-20 11:34:44 -0400

    summer 2026 first load

---
+++ src/editions/summer2026.js
@@ -0,0 +1,38 @@
+export const summer2026 = [
+	{
+		name: "A.A. Balaskovits",
+		slug: "balaskovits",
+		imageSrc: import.meta.env.BASE_URL + "images/summer2026/balaskovits.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Christopher Citro",
+		slug: "citro",
+		imageSrc: import.meta.env.BASE_URL + "images/summer2026/citro.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Nora Nadjarian",
+		slug: "nadjarian",
+		imageSrc: import.meta.env.BASE_URL + "images/summer2026/nadjarian.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Caitlin O’Halloran",
+		slug: "ohalloran",
+		imageSrc: import.meta.env.BASE_URL + "images/summer2026/ohalloran.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Meg Pokrass",
+		slug: "pokrass",
+		imageSrc: import.meta.env.BASE_URL + "images/summer2026/pokrass.jpg",
+		bluesky: "share"
+	},
+	{
+		name: "Mykki Rios",
+		slug: "rios",
+		imageSrc: import.meta.env.BASE_URL + "images/summer2026/rios.jpg",
+		bluesky: "share"
+	},
+];
